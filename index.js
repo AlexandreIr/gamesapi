40607f55403a6e865a288ee66bd9edf34c63836f
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-30 20:37:42 -0300

    feat: add jwt authentication on API

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const port = 3002;
 const Game = require('./model/games/Game');
 const User = require('./model/users/User');
-
-const jwtToken = require('./middlewares/jwtToken');
-
 
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -17,12 +15,36 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
-app.get('/', jwtToken, async (req, res)=>{
+const jwtSecret = 'asfafbhuy1k34208@4&';
+
+function authToken(req, res, next){
+    const jwtoken = req.headers['authorization'];
+    console.log(jwtoken)
+    if(jwtoken!=null){
+        const bearer = jwtoken.split(' ');
+        const token = bearer[1];
+    
+        jwt.verify(token, jwtSecret, (err, data)=>{
+            if(err){
+                res.status(401).json({err:"Token invalido"});
+            } else {
+                req.token = token;
+                req.loggedUser = {id: data.id, email:data.email};
+                console.log(req.loggedUser);
+                next();
+            }
+        });
+    } else {
+        res.status(401).json({err:'Erro, token invalido'});
+    }
+}
+
+app.get('/', authToken, async (req, res)=>{
     const games = await Game.findAll();
-    res.status(200).json(games);
+    res.status(200).json({user:req.loggedUser,games});
 });
 
-app.get('/:id', async(req, res)=>{
+app.get('/:id', authToken, async(req, res)=>{
     const id = req.params.id;
     const game = await Game.findOne({
         where:{
@@ -32,19 +54,23 @@ app.get('/:id', async(req, res)=>{
     res.status(200).json(game);
 });
 
-app.post('/', async (req, res)=>{
+app.post('/',authToken ,async (req, res)=>{
     const {title, price, sinopses, genres, release} = req.body;
-    await Game.create({
-        title,
-        price,
-        sinopses,
-        genres,
-        release
-    });
-    res.status(201).send(`${title} criado com sucesso!`);
+    try{
+        await Game.create({
+            title,
+            price,
+            sinopses,
+            genres,
+            release
+        });
+        res.status(201).send(`${title} criado com sucesso!`);
+    } catch (err){
+        res.status(401).json({err});
+    }
 });
 
-app.put('/:id', async (req, res)=>{
+app.put('/:id', authToken ,async (req, res)=>{
     const id = req.params.id;
     const {title, price, sinopses, genres, release} = req.body;
 
@@ -62,7 +88,7 @@ app.put('/:id', async (req, res)=>{
     res.status(202).send(`jogo de número ${id} atualizado com sucesso!`);
 });
 
-app.delete('/:id', async (req, res)=>{
+app.delete('/:id',authToken ,async (req, res)=>{
     const id = req.params.id;
 
     await Game.destroy({
@@ -73,15 +99,46 @@ app.delete('/:id', async (req, res)=>{
     res.status(204).send("Jogo deletado com sucesso");
 });
 
-app.post('/auth', (req, res)=>{
-    const {email, password} = req.body;
-    jwt.sign({id,email}, jwtSecret, {expiresIn:'24h'}, (err, token)=>{
-        if(err){
-            res.status(400).json({err:"Erro, tente novamente"});
-        } else {
-            res.status(200).json({token});
+app.post('/user', async(req, res)=>{
+    const {name, email, password} = req.body;
+    const userFound = await User.findOne({
+        where:{
+            email:email
         }
     });
+    if(userFound==null){
+        const salt = bcrypt.genSaltSync(11);
+        const hash = bcrypt.hashSync(password, salt);
+        User.create({
+            name,
+            email,
+            password:hash
+        });
+        res.status(201);
+    }
+    res.json({userExists: "Usuário já cadastrado"});
+});
+
+app.get('/user/:id', async(req, res)=>{
+    const id = req.params.id;
+    const userInfo = await User.findOne({where:{id:id}});
+    res.status(200).json(userInfo);
+});
+
+app.post('/auth', async(req, res)=>{
+    const {email, password} = req.body;
+    const user = await User.findOne({where:{email:email}});
+    if(user!=null){
+        const verification = bcrypt.compareSync(password, user.password);
+        if(verification){
+            jwt.sign({id:user.id,email:user.email}, jwtSecret, {expiresIn:'24h'}, (err, token)=>{
+                if(err){
+                    res.status(400).json({err:"Erro, tente novamente"});
+                }
+                res.status(200).json({token});
+            });
+        }
+    }
 });
 
 
